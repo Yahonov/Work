af4b6d5bf1e7b75ce9b652cfc022291b726a163f
«vit» <>
2023-08-06 13:23:40 +0300

    Задание4

--- 4/Program.cs
+++ 4/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+System.Console.WriteLine("Введите число");
+int num1 = Convert.ToInt32(Console.ReadLine());
+int i = 2;
+
+while (num1 >= i)
+{
+    System.Console.Write(i + " ");
+     i = i + 2;
+}
