05bf44c2cef237315040774446b8e310795fffaa
«vit» <>
2023-08-06 13:03:54 +0300

    Задание3

--- 3/Program.cs
+++ 3/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+System.Console.WriteLine("Введите число");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+if(num1 % 2 == 0)
+
+Console.Write("Является четны ");
+
+if(num1 % 2 != 0)
+
+Console.Write("Не является четным");
