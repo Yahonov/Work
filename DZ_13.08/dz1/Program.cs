db9165ef666580a42e12ac6e93eaaeb88a7c6922
«vit» <>
2023-08-12 11:30:06 +0300

    Задание1

---
+++ DZ_13.08/dz1/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+System.Console.WriteLine("Введите трёхзначное число");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+if(num1 < 100 || num1 >= 1000)
+{
+System.Console.WriteLine("Вы ввели не трехзначное число");
+    return;
+}
+
+int num2 = num1 / 10 % 10;
+System.Console.WriteLine($"Вторая цифра{num2}");
+
