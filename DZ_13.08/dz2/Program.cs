2bd9921fd629be4097ce1bf9abb5173ba913a066
«vit» <>
2023-08-12 11:43:11 +0300

    Задание2

--- DZ_13.08/dz2/Program.cs
+++ DZ_13.08/dz2/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+int number = ReadInt("Введите число: ");
+int count = number.ToString().Length;
+Console.Write(MakeArray(number, count));
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int MakeArray(int num1, int num2)
+{
+int result = 0;
+    if (num2 < 3)
+    {
+        Console.Write("Третьей цифры нет ");
+    }
+    else
+    {
+        int num3 = 1;
+        for (int i = num2; i > 3; i--)
+        {
+            num3 = num3 * 10;
+        }
+
+        result = (num1 / num3) % 10;
+    }
+return result;
+}
