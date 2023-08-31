2edc944c9ca2b292eef44fd5557c5718240ccd54
«vit» <>
2023-08-31 13:13:43 +0300

    Задание

---
+++ DZ_02.09/3/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов случайных целых чисел 
+// от 1 до 50 и выводит их на экран.
+
+
+int [] numbers = new int[8];
+Console.Write("[");
+
+for (int i = 0; i < numbers.Length; i++)
+ {
+    numbers [i] = new Random().Next(0, 11);
+    Console.Write(" " + Method (i) + " ");
+ }
+Console.Write("]");
+
+int Method (int a)
+{
+    return numbers[a];
+}
